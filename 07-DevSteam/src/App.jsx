c402e76a71f960a8162af924d95eeb03f3bcc03a
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-04-22 14:17:24 -0300

    Promocard  mockup

--- 07-DevSteam/src/App.jsx
+++ 07-DevSteam/src/App.jsx
@@ -4,11 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
+import Promotion from "./components/Promotion";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Promotion />
     </>
   );
 };
--- 07-DevSteam/src/components/Promotion.jsx
+++ 07-DevSteam/src/components/Promotion.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import PromotionCard from './PromoCard'
 
 const Promotion = () => {
   return (
     <div id='promotion'><h2>Promoções</h2>
-    <div id='itensPromo'></div>
+    <div id='itensPromo'className='d-flex flex-wrap'></div>
+    {/* inserir os cards de promoção  */}
+    <PromotionCard />
+    <PromotionCard />
+    <PromotionCard />
+    <PromotionCard />
     </div>
   )
 }
