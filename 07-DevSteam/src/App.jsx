d22cedef394c8f20687f8bf0b9b042f4c8916e7a
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-04-22 10:19:18 -0300

    Finalizando o header

--- 07-DevSteam/src/App.jsx
+++ 07-DevSteam/src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Header from './components/Header'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
 const App = () => {
   return (
